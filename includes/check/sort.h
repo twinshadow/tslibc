4af38bfacf7d22c0e9d2c325c963cafbc97a89c0
Anthony Cornehl <accornehl@gmail.com>
2013-12-15 01:52:36 +0000

    Add "checksupp", supplementary functions for check-tests
    With code using different methods to reach a common state, it's useful to have
    functions that allow debugging or testing within that space. The sort and hash
    functions have common inputs and states, so this enables myself to write less
    code and more tests.

---
+++ includes/check/sort.h
@@ -0,0 +1,9 @@
+#ifndef CHECK_SORT_H
+#define CHECK_SORT_H
+
+int *random_int_list(int count);
+void print_int_list(int count, int *list);
+int issorted(int count, int *list);
+int compare_int(void *a, void *b);
+
+#endif /* CHECK_SORT_H */
--- src/sort/check_isort.c
+++ src/sort/check_isort.c
@@ -6,65 +6,12 @@
 
 #include "check/twinshadow.h"
 #include "twinshadow/std.h"
+#include "check/sort.h"
 
 int *buf_isort = NULL;
 
 /*****************************************************************************/
 
-int*
-random_int_list(int count)
-{
-	int *buf;
-	int iter;
-	struct timeval time;
-
-	if (count < 1)
-		return NULL;
-
-	buf = calloc(count, sizeof(int));
-	if (buf == NULL)
-		return NULL;
-
-	gettimeofday(&time, NULL);
-	srand((unsigned long)time.tv_usec);
-	for (iter = 0; iter < count; iter++)
-	{
-		buf[iter] = rand() % 20;
-	}
-
-	return buf;
-}
-
-void
-print_int_list(int count, int *list)
-{
-	int iter;
-	for (iter = 0; iter < count; iter++)
-	{
-		printf("%02d ", list[iter]);
-	}
-	puts("");
-}
-
-int
-issorted(int count, int *list)
-{
-	int iter;
-	for (iter = 1; iter < count; iter++)
-		if (list[iter] < list[iter - 1])
-			return 0;
-	return 1;
-}
-
-int
-compare_int(void *a, void *b) {
-	if (*(int*)a == *(int*)b)
-		return 0;
-	else if (*(int*)a < *(int*)b)
-		return -1;
-	else
-		return 1;
-}
 
 /*****************************************************************************/
 
@@ -94,3 +41,5 @@ tcase_isort(void) {
 	tcase_add_test(tc, test_isort);
 	return tc;
 }
+
+CHECK_MAIN_STANDALONE(isort);
